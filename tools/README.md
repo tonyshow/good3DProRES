6d7709530a26dc3e49bda4df8f37022797c8b903
show <show@outllook.com>
2021-01-13 18:46:58 +0800

    更新表格工具

---
+++ tools/README.md
@@ -0,0 +1,11 @@
+# good3DPro
+good3DPro 3D消除
+
+第一步：
+  window:双击 init.sh
+第二步：
+  将石墨表格下载到 in/文件夹下
+第三部:
+  window:双击 createConfig.sh
+第四步：
+  将output文件夹下的allconfig.json上传到远程服务器的/usr/share/nginx/html/remote/下
--- tools/app.js
+++ tools/app.js
@@ -3,7 +3,7 @@ var async = require("async");
 var _ = require('underscore');
 var fs = require('fs');
 var path = require('path');
-const xlsx = require('node-xlsx'); 
+const xlsx = require('node-xlsx');
 const { error } = require("console");
 var app = module.exports = {}
 app.mkdirsSync = function(dirname)
@@ -23,37 +23,37 @@ app.mkdirsSync = function(dirname)
 };
 
 app.main = function()
-{ 
+{
     let all = {};
     app.mkdirsSync('./output/')
-    var list = xlsx.parse("./in/关卡系统.xlsx"); 
+    var list = xlsx.parse("./in/关卡系统.xlsx");
     if (_.size(list) > 0)
     {
         _.each( list , (info,idx)=>{
-            error("name")
+            error(info.name)
             let name = info.name;
             let data = info.data;
 
             let tbData= 0;
             if("level" === name){
                 tbData = app.parsList(data,5)
-            }else if("parameter" === name){ 
+            }else if("parameter" === name){
                 tbData = app.parsparameter(data)
             }else if("passGold" === name){
-                tbData = app.parsList(data,3) 
-            }else if("passStar" === name){ 
-                tbData = app.parsList(data,2) 
-            }else if("buff" === name){ 
-                tbData = app.parsList(data,2) 
-            } 
+                tbData = app.parsList(data,3)
+            }else if("passStar" === name){
+                tbData = app.parsList(data,2)
+            }else if("buff" === name){
+                tbData = app.parsList(data,2)
+            }
             all[name]=tbData
         } )
         fs.writeFileSync('./output/allconfig.json',JSON.stringify(all));
-    } 
-} 
+    }
+}
 
 app.parsparameter = function(list){
-    let data = {}; 
+    let data = {};
     _.each( list , (info , idx)=>{
         if( idx >=2 && info.length==3 ){
             data[info[0]]=info[1]
@@ -62,7 +62,7 @@ app.parsparameter = function(list){
     return data
 }
 app.parsList = function(list,length){
-    let data = []; 
+    let data = [];
     _.each( list , (info , idx)=>{
         if( idx >=2 && info.length==length ){
             data.push(info)
