ebb5397f6fec1511e431fc519256081a659245b0
show <show@outllook.com>
2021-03-18 16:26:45 +0800

    开启1.0.3

--- tools/createConfig.sh
+++ tools/createConfig.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 node createConfig.js
 cd output
-scp allconfig.json  root@47.102.37.73:/usr/share/nginx/html/1.0.2/remote/
+scp allconfig.json  root@47.102.37.73:/usr/share/nginx/html/1.0.3/remote/
